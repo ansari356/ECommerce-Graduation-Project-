bcf4f11619ea2f52cbde39c0b05561db59f15f72
Muhammad Alansari <muhammad.al.ansari356@gmail.com>
2024-12-03 23:28:42 +0200

    second commit

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ import initApp from './src/index.router.js'
 const app = express()
 const port = process.env.PORT || 5000
 app.set('case sensitive routing', true);
-
+//dsadasdasdad
 initApp(app, express)
 app.listen(port, () => console.log(chalk.blue(`Example app listening on port`) +" "+ chalk.green(`${port}!`)))
